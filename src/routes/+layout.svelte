56a356e3177b7c93f68553e37732d8a359beb5f8
Aman-web-dev <amanzhx1234+github@gmail.com>
2025-04-10 22:27:19 +0530

    New Sevelte App Initiated

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,54 @@
+<script lang="ts">
+	import '../app.css';
+	import Chatcard from '$lib/images/custom-components/chatcard/+chatcard.svelte';
+	import Navbar from '$lib/images/custom-components/navbar/+navbar.svelte';
+	
+	let { children } = $props();
+</script>
+
+<div class="app">
+	<Navbar></Navbar>
+
+	<main>
+		{@render children()}
+	</main>
+
+
+</div>
+
+<style>
+	.app {
+		display: flex;
+		flex-direction: column;
+		min-height: 100vh;
+	}
+
+	main {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		padding: 1rem;
+		width: 100%;
+		max-width: 64rem;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+
+	footer {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		padding: 12px;
+	}
+
+	footer a {
+		font-weight: bold;
+	}
+
+	@media (min-width: 480px) {
+		footer {
+			padding: 12px 0;
+		}
+	}
+</style>
